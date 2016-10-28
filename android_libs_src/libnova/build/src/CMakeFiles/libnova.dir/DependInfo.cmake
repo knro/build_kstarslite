5c0b749804fca6164610799e7746f443fdaad424
Jasem Mutlaq <mutlaqja@ikarustech.com>
2016-10-28 21:15:36 +0300

    Initial import

---
+++ android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/aberration.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/aberration.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/airmass.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/airmass.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/angular_separation.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/angular_separation.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/apparent_position.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/apparent_position.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/asteroid.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/asteroid.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/comet.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/comet.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/dynamical_time.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/dynamical_time.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/earth.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/earth.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/elliptic_motion.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/elliptic_motion.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/heliocentric_time.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/heliocentric_time.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/hyperbolic_motion.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/hyperbolic_motion.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/julian_day.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/julian_day.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/jupiter.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/jupiter.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/lunar.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/lunar.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/mars.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/mars.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/mercury.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/mercury.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/neptune.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/neptune.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/nutation.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/nutation.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/parabolic_motion.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/parabolic_motion.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/parallax.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/parallax.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/pluto.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/pluto.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/precession.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/precession.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/proper_motion.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/proper_motion.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/refraction.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/refraction.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/rise_set.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/rise_set.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/saturn.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/saturn.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/sidereal_time.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/sidereal_time.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/solar.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/solar.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/transform.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/transform.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/uranus.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/uranus.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/utility.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/utility.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/venus.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/venus.c.o"
+  "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/src/vsop87.c" "/home/jasem/Projects/kstars/build_kstarslite/android_libs_src/libnova/build/src/CMakeFiles/libnova.dir/vsop87.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ANDROID"
+  "LIBNOVA_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/jasem/Projects/android/android-ndk-r13/sources/cxx-stl/gnu-libstdc++/4.9/libs/armeabi-v7a/include"
+  "/home/jasem/Projects/android/android-ndk-r13/sources/cxx-stl/gnu-libstdc++/4.9/include"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
